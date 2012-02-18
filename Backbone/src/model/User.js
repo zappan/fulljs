33fc537d41b0c3ff22b9a5c7e803f3e118a212a1
bsutic <bruno.sutic@gmail.com>
2012-02-18 20:12:37 +0100

    Added user update

--- Backbone/src/model/User.js
+++ Backbone/src/model/User.js
@@ -1,3 +1,27 @@
 MyApplication.Model.User = Backbone.Model.extend({
-  //...
+  
+  idAttribute: "_id",
+  
+  _isStateEdit: false,
+  
+  isInEditState: function() {
+    return this._isStateEdit;
+  },
+  
+  enterEditState: function() {
+    if ( this.isInEditState() )    // checks edit state
+      return;
+
+    this._isStateEdit = true;
+    this.trigger('change');
+  },
+  
+  leaveEditState: function() {
+    if ( !this.isInEditState() )    // checks edit state
+      return;
+      
+    this._isStateEdit = false;
+    this.trigger('change');
+  }
+  
 });
--- Backbone/src/view/User.js
+++ Backbone/src/view/User.js
@@ -1,13 +1,56 @@
 MyApplication.View.User = Backbone.View.extend({
   initialize: function () {
-    _.bindAll(this,'render');
+    _.bindAll(this, 'render', 'click_leaveEditState');
     this.model.bind('change', this.render);
   },
   
+  events:{
+    'click .edit-button' : 'click_enterEditState',
+    'click .cancel-button' : 'click_leaveEditState',
+    'submit #edit-user' : 'click_submit'
+  },
+  
   render: function () {
-    var renderedContent = ich.user_template(this.model.toJSON());   // fills the template with values
+    // ** this works by using function pointer to function template
+    // var fnTemplate = this.model.isInEditState()
+    //     ? ich.user_template_edit
+    //     : ich.user_template
+    //   , renderedContent = fnTemplate(this.model.toJSON());
+    // 
+    // 
+    // ** model to json saved in var to keep stuff DRY
+    // var modelJSON = this.model.toJSON()
+    //   , renderedContent = this.model.isInEditState()
+    //     ? ich.user_template_edit(modelJSON)
+    //     : ich.user_template(modelJSON);
+
+    var renderedContent = this.model.isInEditState()
+      ? ich.user_edit_template(this.model.toJSON())
+      : ich.user_template(this.model.toJSON());
+        
     this.$el.html(renderedContent);                                 // attachs to parent? => [TC] attaches rendered HTML to the view's top-level DOM element
     return this;                                                    // enables chaining
+  },
+  
+  click_enterEditState: function() {
+    this.model.enterEditState();
+  },
+  
+  click_leaveEditState: function() {
+    this.model.leaveEditState();
+  },
+  
+  click_submit: function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    var thisView = this
+      , email = this.$("input[name='email']").val();
+    
+    this.model.set({email: email});
+    this.model.save(null, {
+        success: thisView.click_leaveEditState
+    });
   }
   
 });
