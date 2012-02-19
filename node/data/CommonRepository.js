23c11225655be4ee115cc1119e730585b2294e67
Tomislav Capan <tomislav.capan@gmail.com>
2012-02-19 01:12:18 +0100

    Repositories prototypal inheritance

--- node/data/CommonRepository.js
+++ node/data/CommonRepository.js
@@ -1,41 +1,52 @@
-var db = require("../data/_dbConfig").db
-  , ObjectId = require("../data/_dbConfig").ObjectId;
+var ObjectId = require("../data/_dbConfig").ObjectId;
 
 CommonRepository = function() {};
 
 // findAll
-CommonRepository.prototype.findAll = function(dbCollection, callback) {
-  dbCollection.find({}, function(err, models) {
+CommonRepository.prototype.findAll = function(callback) {
+  if (!this.dbCollection || !this.dbCollection.find)
+    return callback("Fatal error! Missing repository's dbCollection");
+  
+  this.dbCollection.find({}, function(err, models) {
     if (err) return callback(err)
     else callback(null, models);
   });
 };
 
 // findById
-CommonRepository.prototype.findById = function(dbCollection, id, callback) {
-  dbCollection.findOne({ _id : id }, function(err, models) {
+CommonRepository.prototype.findById = function(id, callback) {
+  if (!this.dbCollection || !this.dbCollection.findOne)
+    return callback("Fatal error! Missing repository's dbCollection");
+  
+  this.dbCollection.findOne({ _id : id }, function(err, models) {
     if (err) return callback(err)
     else callback(null, models);
   });
 };
 
 // save
-CommonRepository.prototype.save = function(dbCollection, model, callback) {
-  dbCollection.insert(model, function(err, result) {
+CommonRepository.prototype.save = function(model, callback) {
+  if (!this.dbCollection || !this.dbCollection.save)
+    return callback("Fatal error! Missing repository's dbCollection");
+  
+  this.dbCollection.insert(model, function(err, result) {
     if (err) return callback(err)
     else callback(null, model);
   });
 };
 
 // update
-CommonRepository.prototype.update = function(dbCollection, model, callback) {
+CommonRepository.prototype.update = function(model, callback) {
+  if (!this.dbCollection || !this.dbCollection.update)
+    return callback("Fatal error! Missing repository's dbCollection");
+  
   var modelId = ObjectId(model._id);
   delete model._id;
 
-  dbCollection.update({ "_id" : modelId }, { $set : model }, function(err, result) {
+  this.dbCollection.update({ "_id" : modelId }, { $set : model }, function(err, result) {
     if (err || !result) return callback(err)
     else callback(null, model);
   });
 };
 
-exports.Repository = new CommonRepository();
+exports.CommonRepository = CommonRepository;
--- node/data/UsersRepository.js
+++ node/data/UsersRepository.js
@@ -1,27 +1,9 @@
 var db = require("../data/_dbConfig").db
-  , ObjectId = require("../data/_dbConfig").ObjectId
-  , commonRepository = require("./CommonRepository").Repository;
+  , CommonRepository = require("./CommonRepository").CommonRepository;
 
 UsersRepository = function() {};
-
-// findAll
-UsersRepository.prototype.findAll = function(callback) {
-  commonRepository.findAll(db.users, callback);
-};
-
-// findById
-UsersRepository.prototype.findById = function(id, callback) {
-  commonRepository.findById(db.users, callback);
-};
-
-// save
-UsersRepository.prototype.save = function(user, callback) {
-  commonRepository.save(db.users, user, callback);
-};
-
-// update
-UsersRepository.prototype.update = function(user, callback) {
-  commonRepository.update(db.users, user, callback);
-};
+UsersRepository.prototype = new CommonRepository;
+UsersRepository.prototype.constructor = UsersRepository;
+UsersRepository.prototype.dbCollection = db.users;
 
 exports.Repository = new UsersRepository();
