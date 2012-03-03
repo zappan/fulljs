d3020b2f6070f3df8e1a539b4835ea9be28faa4d
Tomislav Capan <tomislav.capan@gmail.com>
2012-03-03 20:19:58 +0100

    Added support for db.config using env vars with fallback to local config file

--- node/data/_dbConfig.js
+++ node/data/_dbConfig.js
@@ -1,4 +1,13 @@
-var dbConfig = require("../data/_db.config").dbConfig
+var dbDevConfig = require("../data/_db.config").dbConfig
+
+// config hash using env variables with fallback to dev config file
+var dbConfig = {
+        name : process.env['dbname']     || dbDevConfig.name
+      , host : process.env['dbhost']     || dbDevConfig.host
+      , port : process.env['dbport']     || dbDevConfig.port
+      , user : process.env['dbusername'] || dbDevConfig.user
+      , pass : process.env['dbpassword'] || dbDevConfig.pass
+    };
 
     // "username:password@example.com/mydb"
 var databaseUrl =       dbConfig.user + ':' + dbConfig.pass 
