dd7cc23ade9bf6a0c808a3a1e70e1bfb953b0d00
Tomislav Capan <tomislav.capan@gmail.com>
2012-08-27 23:39:26 +0200

    Removing Sinatra dependencies

--- config.ru
+++
@@ -1,3 +0,0 @@
-require './sinatra/server'
-
-run Sinatra::Application
--- lib/init.rb
+++ lib/init.rb
@@ -1,12 +1,1 @@
-Dir['./lib/isolate*/lib'].each do |dir|
-  $: << dir
-end
-
-#require "rake/dsl_definition"
-#require "rake"
-#require "juicer"
-
-#require "rubygems"
-require "isolate/now"
-require "sinatra"
 
